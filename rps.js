6753677d43bf4e00893d13dc1356e4cf91a89cbf
hotelcity1997 <james.sinclair808@gmail.com>
2022-01-29 02:11:05 -0600

    initial project upload

---
+++ rps.js
@@ -0,0 +1,45 @@
+//       computerVictories = (computerVictories + 1)
+
+let playerVictories = 0;
+let computerVictories = 0;
+
+game();
+
+function game() {
+
+  playRound();
+  playRound();
+  playRound();
+  playRound();
+  playRound();
+  if (playerVictories == computerVictories) {
+    alert(`You tied ${playerVictories} - ${computerVictories}!`)
+  } else if (playerVictories > computerVictories) {
+    alert(`You won ${playerVictories} - ${computerVictories}!`)
+  } else {
+    alert(`You lost ${computerVictories} - ${playerVictories}!`)  
+  }
+}
+
+function playRound(playerSelection, computerSelection) {
+
+  let computerChoices = ["rock", "paper", "scissors"];
+  computerSelection = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  console.log(computerSelection);
+  playerSelection = prompt("Type rock, paper, or scissors:", '');
+  playerSelection = playerSelection.toLowerCase();
+  if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
+         alert("You must type \"rock\", \"paper\", or \"scissors\".");
+         playRound();
+     } else if
+       (playerSelection == computerSelection) {
+       alert("It's a tie. Boring.");
+     } else if
+       (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper") {
+       alert(`The computer chose ${computerSelection}. Hot damn, you win!`);
+       playerVictories = (playerVictories + 1);
+     } else {
+       alert(`The computer chose ${computerSelection}. You lose. I'm so sorry. So very, very sorry.`);
+       computerVictories = (computerVictories + 1);
+     }
+     }
