43c3697c7c011bad2cfa318b4adad446cfb44f7d
hotelcity1997 <james.sinclair808@gmail.com>
2022-02-13 12:50:29 -0600

    initial UI version upload

--- rps.js
+++ rps.js
@@ -1,44 +1,77 @@
 let playerVictories = 0;
 let computerVictories = 0;
 
-game();
+const results = document.querySelector('.results')
+const playerVictoriesP = document.querySelector('.player-victories')
+const computerVictoriesP = document.querySelector('.computer-victories')
 
-function game() {
-  playRound();
-  playRound();
-  playRound();
-  playRound();
-  playRound();
-  if (playerVictories == computerVictories) {
-    alert(`Final score: You tied ${playerVictories} - ${computerVictories}.`)
-  } else if (playerVictories > computerVictories) {
-    alert(`Final score: You won ${playerVictories} - ${computerVictories}!`)
-  } else {
-    alert(`Final score: You lost ${computerVictories} - ${playerVictories}.`)  
-  }
-}
-
-function playRound(playerSelection, computerSelection) {
-
-  let computerChoices = ["rock", "paper", "scissors"];
-  computerSelection = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-  console.log(computerSelection);
-  playerSelection = prompt("Type rock, paper, or scissors:", '');
-  playerSelection = playerSelection.toLowerCase();
-  if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
-    alert(`You entered "${playerSelection}". You must type \"rock\", \"paper\", or \"scissors\".`);
+let playerSelection = document.querySelectorAll('button');
+console.log(playerSelection);
+  
+playerSelection.forEach((button) => {
+  button.addEventListener('click', () => {
+    playerSelection = button.id;
+    console.log(playerSelection);
     playRound();
-  } else if
-    (playerSelection == computerSelection) {
-    alert("It's a tie. Boring.");
+   })
+ });
+
+function playRound() {
+
+  const computerChoices = ["rock", "paper", "scissors"];
+  const computerSelection = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+
+  if (playerSelection == computerSelection) {
+    computerVictoriesP.classList.add('hide');
+    playerVictoriesP.classList.add('hide');
+    results.textContent = "It's a tie. Boring.";
+    setTimeout( () => {
+      playerVictoriesP.textContent = playerVictories;
+      computerVictoriesP.textContent = computerVictories;
+      playerVictoriesP.classList.remove('hide')
+      computerVictoriesP.classList.remove('hide')
+    }, 300);
+    
   } else if
     (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" &&
-        computerSelection == "rock" ||playerSelection == "scissors" && computerSelection == "paper") {
-    alert(`The computer chose ${computerSelection}. Hot damn, you win!`);
-    playerVictories = (playerVictories + 1);
+      computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper") {
+        
+        playerVictoriesP.classList.add('hide');
+        results.textContent = `The computer chose ${computerSelection}. Hot damn, you win!`;
+        playerVictories += 1;
+    setTimeout( () => {
+      playerVictoriesP.classList.remove('hide')
+      playerVictoriesP.textContent = playerVictories;
+      computerVictoriesP.textContent = computerVictories;
+    }, 300);
+    
   } else {
-    alert(`The computer chose ${computerSelection}. You lose. I'm so very, very sorry.`);
-    computerVictories = (computerVictories + 1);
+    
+    computerVictoriesP.classList.add('hide');
+    
+    results.textContent = `The computer chose ${computerSelection}. You lose. I'm so very, very sorry.`;
+    computerVictories += 1;
+    setTimeout( () => {
+      computerVictoriesP.classList.remove('hide')
+      computerVictoriesP.textContent = computerVictories;
+      playerVictoriesP.textContent = playerVictories;
+    }, 300);
+    
+  }
+  if (computerVictories == 5) {
+    results.textContent = "YOU LOSE. GOOD DAY SIR."
+    setTimeout( () => {
+      playerVictories = 0;
+      computerVictories = 0;
+    }, 300);
+  }
+  if (playerVictories == 5) {
+    results.textContent = "You won the best of 5 rounds!"
+    setTimeout( () => {
+      playerVictories = 0;
+      computerVictories = 0;
+    }, 300);
   }
 }
 
